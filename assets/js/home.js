9872b9b7cea1a82fe175ddc169a78ab97d300085
Noah <najahn19@gmail.com>
2019-02-09 19:16:46 -0500

    Reworked navbar, added 'pages' for projects and resume

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,8 +1,25 @@
-function writeHome() {
-    document.write("hello world");
-    console.log("test");
+function initialLoad() {
+    displayQuote();
 }
 
-function displayAbout() {
-    console.log("about");
+function displayQuote() {
+    var quotes = [  "\"Fear is the path to the dark side.\" &#8210 Yoda",
+                    "\"With great power, comes great responsibility.\" &#8210 Uncle Ben",
+                    "\"The Force will be with you. Always.\" &#8210 Obi-Wan Kenobi",
+                ];
+
+    var navQuote = document.getElementById("quote");
+    navQuote.innerHTML = ("<i>" + quotes[Math.floor(Math.random() * (quotes.length))] + "</i>");
 }
+
+function addActiveToNav(makeActive) {
+    var navs = document.getElementsByClassName("nav-link active");
+
+    for (i = 0; i < navs.length; i++) {
+        navs[i].classList.remove("active");
+    }
+
+    makeActive.classList.add("active");
+}
+
+window.onload = initialLoad;
