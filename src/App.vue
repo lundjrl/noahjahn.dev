8c8bec4004b2b5c1be8cd636356ebb8d8309bf94
Noah Jahn <najahn19@gmail.com>
2021-05-06 23:38:06 -0400

    Setup vue app

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,25 @@
 <template>
-  <div>
-    <Terminal />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import Terminal from './components/Terminal.vue';
-
+import HelloWorld from './components/HelloWorld.vue';
 export default {
   name: 'App',
   components: {
-    Terminal,
+    HelloWorld,
   },
 };
 </script>
 
 <style lang="scss">
-html {
-  background-color: #151515;
-  font-family: 'PT Mono', monospace;
-  font-size: 28px;
-  color: #E1E1E1;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-}
-
-html, body {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  width: 100%;
-  overflow: hidden;
-}
-
-#app {
-  margin-top: 1rem;
-  margin-bottom: 1rem;
-  margin-left: auto;
-  margin-right: auto;
-  max-width: 50%;
-  display: block;
-  position: absolute;
-  height: auto;
-  bottom: 0;
-  top :0;
-  left:0;
-  right:0;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
