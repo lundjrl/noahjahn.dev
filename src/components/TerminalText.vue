5528acb1813525f1279e9ace20c7d71dfd4de6be
Noah Jahn <najahn19@gmail.com>
2021-07-05 13:53:11 -0400

    Setup TerminalText compontent to type with animation

--- src/components/TerminalText.vue
+++ src/components/TerminalText.vue
@@ -1,14 +1,57 @@
 <template>
   <p>
-    $ {{ text }}_
+    $ {{ text }}|
   </p>
 </template>
 
 <script>
+
+// const aText = [
+//   'There are only 10 types of people in the world:',
+//   'Those who understand binary, and those who don\'t',
+// ];
+// const iSpeed = 100; // time delay of print out
+// const iIndex = 0; // start printing array at this posision
+// const iArrLength = aText[0].length; // the length of the text array
+// const iScrollAt = 20; // start scrolling up at this many lines
+
+// const iTextPos = 0; // initialise text position
+// const sContents = ''; // initialise contents variable
+// let iRow; // initialise current row
+
+function type(text) {
+  console.log(text);
+  // sContents = ' ';
+  // iRow = Math.max(0, iIndex - iScrollAt);
+  // const destination = document.getElementById('typedtext');
+
+  // while (iRow < iIndex) {
+  //   sContents += `${aText[iRow += 1]}<br />`;
+  // }
+  // destination.innerHTML = `${sContents + aText[iIndex].substring(0, iTextPos)}_`;
+  // // eslint-disable-next-line no-plusplus
+  // if (iTextPos++ === iArrLength) {
+  //   iTextPos = 0;
+  //   iIndex += 1;
+  //   if (iIndex !== aText.length) {
+  //     iArrLength = aText[iIndex].length;
+  //     setTimeout(typewriter, 500);
+  //   }
+  // } else {
+  //   setTimeout(typewriter, iSpeed);
+  // }
+}
+
 export default {
   name: 'TerminalText',
   props: {
     text: String,
+  },
+  data: (instance) => {
+    type(instance.text);
+  },
+  methods: {
+    type,
   },
 };
 </script>
