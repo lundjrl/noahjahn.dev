fdc3298d6c1b6d39bf590eebeecf0f6b0b6bce81
Noah Jahn <najahn19@gmail.com>
2021-07-06 10:01:16 -0400

    Updated start script to make it easier to stop the containers

--- bin/start.sh
+++ bin/start.sh
@@ -3,4 +3,5 @@
 export UID
 docker-compose pull || exit
 docker-compose build || exit
-docker-compose up || exit
+docker-compose up -d || exit
+docker-compose logs -f || { docker-compose down; exit; }
